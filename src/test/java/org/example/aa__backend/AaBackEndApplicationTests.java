58f70230df40f967c640790b6e52f5815a228601
kimhanju <s24043@gsm.hs.kr>
2025-12-18 11:44:23 +0900

    main

---
+++ src/test/java/org/example/aa__backend/AaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.aa__backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
