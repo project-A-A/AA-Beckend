49a7b6087c57ee6e3a63daac30ae6f8ed1225ed9
kimhanju <s24043@gsm.hs.kr>
2025-12-18 15:00:59 +0900

    main

---
+++ src/main/java/org/example/aa__backend/AaBackEndApplication.java
@@ -0,0 +1,12 @@
+package org.example.aa__backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AaBackEndApplication.class, args);
+    }
+}
